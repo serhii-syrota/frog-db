54e16fe893e85df9d79643a0451e9c7953af3d26
Serhii <sergey.crisp@gmail.com>
2022-10-12 18:49:51 +0300

    remove env vars, use hardcoded

--- src/bin/daemon/main.go
+++ src/bin/daemon/main.go
@@ -5,16 +5,9 @@ import (
 	"log"
 	"time"
 
-	"github.com/caarlos0/env"
 	"github.com/ssyrota/frog-db/src/core/db"
 	"github.com/ssyrota/frog-db/src/web"
 )
-
-type Config struct {
-	Path         string        `env:"DUMP_PATH" envDefault:".dump.json"`
-	DumpInterval time.Duration `env:"DUMP_INTERVAL" envDefault:"1m"`
-	Port         int           `env:"REST_PORT" envDefault:"8080"`
-}
 
 func main() {
 	if err := load(); err != nil {
@@ -23,15 +16,14 @@ func main() {
 }
 
 func load() error {
-	cfg := Config{}
-	if err := env.Parse(&cfg); err != nil {
-		return fmt.Errorf("config: %w", err)
-	}
-	db, err := db.New(cfg.Path, cfg.DumpInterval)
+	port := 8080
+	dumpPath := ".dump.json"
+	dumpInterval := time.Second * 100
+	db, err := db.New(dumpPath, dumpInterval)
 	if err != nil {
 		return fmt.Errorf("init db: %w", err)
 	}
-	if err := web.New(db, uint16(cfg.Port)).Run(); err != nil {
+	if err := web.New(db, uint16(port)).Run(); err != nil {
 		return fmt.Errorf("run rest: %w", err)
 	}
 	return nil
